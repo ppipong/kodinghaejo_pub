98c01d3d93f5f0a570ebcccaabc42099bac02057
Adming <ppipong@naver.com>
2024-11-01 16:47:10 +0900

    --241101join

--- js/pub-ui-custom.js
+++ js/pub-ui-custom.js
@@ -1,4 +1,5 @@
 /* pub-js */
+
 fetch("/include/header.html")
   .then(response => {
     return response.text()
@@ -15,17 +16,19 @@ fetch("/include/footer.html")
     document.querySelector("#footer").innerHTML = data;
   });
 
-window.onload = () => {
-  if (document.querySelector('.main-navbar')) {
-    document.addEventListener('mouseover', function(event) {
-      if (event.target.closest('.main-list-item .nav-hover') || event.target.closest('.main-nav-list-menu')) {
-        document.querySelector('.main-nav-list-menu').classList.add('active');
-      } else if (!event.target.closest('.main-navbar')) {
-        document.querySelector('.main-nav-list-menu').classList.remove('active');
-      }
-    });
+  window.onload = () => {
+    if (document.querySelector('.main-navbar')) {
+      document.addEventListener('mouseover', function(event) {
+        if (event.target.closest('.main-list-item .nav-hover') || event.target.closest('.main-nav-list-menu')) {
+          document.querySelector('.main-nav-list-menu').classList.add('active');
+          document.querySelector('.main-nav-list-menu').style.zIndex = 1000;
+        } else if (!event.target.closest('.main-navbar')) {
+          document.querySelector('.main-nav-list-menu').classList.remove('active');
+          document.querySelector('.main-nav-list-menu').style.zIndex = 0;
+        }
+      });
+    }
   }
-}
 
 document.addEventListener('click', function(event) {
   if (event.target.matches('.xi-bookmark-o') || event.target.matches('.xi-bookmark')) {
@@ -41,5 +44,10 @@ document.addEventListener('click', function(event) {
   }
 });
 
-
+document.addEventListener('click', function(event) {
+  if (event.target.matches('.xi-heart-o') || event.target.matches('.xi-heart')) {
+    event.target.classList.toggle("xi-heart-o");
+    event.target.classList.toggle("xi-heart");
+  }
+});
 
