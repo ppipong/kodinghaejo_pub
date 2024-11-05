5623be1c202363dd4cebade017161f952e499c01
Adming <ppipong@naver.com>
2024-11-05 09:50:25 +0900

    --1105--오전

--- js/pub-ui-custom.js
+++ js/pub-ui-custom.js
@@ -47,3 +47,17 @@ const loginView = (e) => {
 }
 
 const loginChk = () => {};
+
+document.addEventListener('click', function(event) {
+  if (event.target.matches('.xi-bookmark-o') || event.target.matches('.xi-bookmark')) {
+    event.target.classList.toggle("xi-bookmark-o");
+    event.target.classList.toggle("xi-bookmark");
+  }
+});
+
+document.addEventListener('click', function(event) {
+  if (event.target.matches('.xi-heart-o') || event.target.matches('.xi-heart')) {
+    event.target.classList.toggle("xi-heart-o");
+    event.target.classList.toggle("xi-heart");
+  }
+});
