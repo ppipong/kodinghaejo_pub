2f9d8968dc6a6b526cc7ce2c2c033e14744477f3
ming home <ppipong_@naver.com>
2024-11-07 01:04:58 +0900

    --1107-파일구조변경

--- js/pub-ui-custom.js
+++ js/pub-ui-custom.js
@@ -80,12 +80,6 @@ function listDropdown() {
   });
 }
 
-
-
-
-
-
-
 document.addEventListener('click', function(event) {
   if (event.target.matches('.xi-bookmark-o') || event.target.matches('.xi-bookmark')) {
     event.target.classList.toggle("xi-bookmark-o");
