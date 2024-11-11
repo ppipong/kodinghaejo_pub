63c99d18de9043f9b8a1ad5cde00706bf4d47a65
ming home <ppipong_@naver.com>
2024-11-12 00:20:05 +0900

    1112--pagenation

--- js/pub-ui-custom.js
+++ js/pub-ui-custom.js
@@ -23,7 +23,7 @@ fetch("/include/header.html")
         }
       });
     }
-    /* // header : 드롭다운 */
+    /* // header : 드롭다운 -- 타임리프 처리시 해더 영역에 복붙 */
   });
 
 fetch("/include/footer.html")
@@ -70,14 +70,30 @@ const loginView = (e) => {
 const loginChk = () => {};
 */
 
-/* 문제 /problemCollect : 드롭다운 */
-const rank_tab = document.getElementById('#rank_tab');
-console.log(rank_tab);
+/* 공통 - 페이지 네이션 : on class 이펙트 */
+function pagenation() {
+  const paginateContainers = document.querySelectorAll('.paginate');
+  paginateContainers.forEach(container => {
+    // 각 페이지네이션 컨테이너에서 페이지 번호 링크들만 선택
+    const pageLinks = container.querySelectorAll('a[href="#"]');
 
+    pageLinks.forEach(link => {
+      link.addEventListener('click', function(e) {
+        // 'on' 클래스를 제거하는 부분 (현재 페이지네이션에서만 처리)
+        pageLinks.forEach(link => link.classList.remove('on'));
+
+        // 클릭한 페이지 링크에 'on' 클래스를 추가
+        this.classList.add('on');
+      });
+    });
+  });
+}
+
+/* 문제 /problemCollect : 드롭다운 -- 느린 것 수정 해야됨 */
 function listDropdown() {
+  const rank_tab = document.getElementById('#rank_tab');
   const itemli = document.querySelectorAll('.toggle_btn');
-  // const innerArea = document.querySelector('.list-box-content');
-  
+  console.log(rank_tab);
   itemli.forEach(function (item) {
     item.addEventListener('click', function () {
       // 현재 클릭한 요소에 'on' 클래스 토글
@@ -91,6 +107,30 @@ function listDropdown() {
       });
     });
   });
+}
+
+/* 랭크 /rank : 탭 */
+function toggleTab() {
+  const btnMenu = document.querySelectorAll('.rank_tab_group li');
+  const menuContent = document.querySelectorAll('.content-rank-main .content-rank-toggle-menu-item')
+  let menuCount = '';
+  
+  for(let i = 0; i < btnMenu.length; i++){
+    btnMenu[i].querySelector('.btn_tab_menu').addEventListener('click', function(e){
+        e.preventDefault();
+        for(let j = 0; j < btnMenu.length; j++){
+          // 나머지 버튼 클래스 제거
+          btnMenu[j].classList.remove('on');
+          // 나머지 컨텐츠 display:none 처리
+          menuContent[j].style.display = 'none';
+        }
+        // 버튼 관련 이벤트
+        this.parentNode.classList.add('on');
+        // 버튼 클릭시 컨텐츠 전환
+        menuCount = this.getAttribute('href');
+        document.querySelector(menuCount).style.display = 'block';
+    });
+  }
 }
 
 document.addEventListener('click', function(event) {
